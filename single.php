c2a5da78215759a02ba136a83e68b2c45f3adfd2
ltr3 <ltr3@0247d734-89a4-485f-bd0c-6678ce468551>
2011-12-12 19:26:25 +0000

    Only display Related Content and Related Content Image if they are present

--- single.php
+++ single.php
@@ -2,10 +2,16 @@
 add_action('genesis_after_post_content', 'lr_related');
 
 function lr_related(){
-  if(genesis_get_custom_field('_lr_related_title') !== 'none') { ?>
+  $related_title = genesis_get_custom_field('_lr_related_title');
+  $related_image = genesis_get_custom_field('_lr_related_image');
+  if ($related_title == '') { ?>
+    <!-- no related -->
+  <?php } else { ?> 
   <div id="related-content">
     <h3><a href="<?php echo genesis_get_custom_field('_lr_related_link'); ?>"><?php echo genesis_get_custom_field('_lr_related_title'); ?></a></h3>
-    <?php if(genesis_get_custom_field('_lr_related_image') !== 'none') { ?>
+    <?php if($related_image == '') { ?>
+    <!-- no image -->
+  <?php } else { ?> 
       <a href="<?php echo genesis_get_custom_field('_lr_related_link'); ?>"><img alt="<?php echo genesis_get_custom_field('_lr_related_title'); ?> Image" src="<?php echo genesis_get_custom_field('_lr_related_image'); ?>"></a>
     <?php } ?>
     <p class="related-description"><?php echo genesis_get_custom_field('_lr_related_description'); ?></p>
